c42a75b99fde4afaf9cfc2167ba208bc1d148438
vlat <email2vlat@gmail.com>
2018-09-25 21:27:11 +0300

    fix

--- shether2/src/comp/shared/list/AccountRow_D.js
+++ shether2/src/comp/shared/list/AccountRow_D.js
@@ -7,15 +7,15 @@ import Link from "react-router-dom/es/Link";
 
 export class AccountRow extends React.Component {
     render() {
-            let url = 'https://ropsten.etherscan.io/address/' + this.props.username;
+            let url = 'https://ropsten.etherscan.io/address/' + this.props.address;
             let style = this.props.hasaction ? {backgroundColor: '#fff5c6'} : undefined;
             let titleBtn = this.props.hasaction ? 'Перейти к фото' : 'Начислить';
             switch (this.props.mode) {
                 case 'admin':
                     return (
                         <TableRow style={style}>
-                            <TableCell className={'sketch-tbody'}><a href={url} target={'_blank'}>{this.props.username}</a></TableCell>
-                            <TableCell className={'sketch-tbody'}>{this.props.address}</TableCell>
+                            <TableCell className={'sketch-tbody'}>{this.props.username}</TableCell>
+                            <TableCell className={'sketch-tbody'}><a href={url} target={'_blank'}>{this.props.address}</a></TableCell>
                             <TableCell className={'sketch-tbody'} numeric>{this.props.balance}</TableCell>
                             <TableCell className={'sketch-tbody'}>{this.props.created}</TableCell>
                             <TableCell className={'sketch-tbody'}><Button variant="outlined" onClick={() => {this.props.handleReview(this.props);}}>{titleBtn}</Button></TableCell>
@@ -25,7 +25,7 @@ export class AccountRow extends React.Component {
                     return (
                         <TableRow>
                             <TableCell className={'sketch-tbody'}>{this.props.username}</TableCell>
-                            <TableCell className={'sketch-tbody'}>{this.props.address}</TableCell>
+                            <TableCell className={'sketch-tbody'}><a href={url} target={'_blank'}>{this.props.address}</a></TableCell>
                             <TableCell className={'sketch-tbody'} numeric>{this.props.balance}</TableCell>
                         </TableRow>
                     );
